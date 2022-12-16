e928adc82ea705ca692114bada64409d64c15dac
Keila Lopez <keilalofra@gmail.com>
2022-12-15 22:03:47 -0600

    only 2 tests failing

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,6 +1,10 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :title, :content
+  attributes :title, :short_content
    
   belongs_to :author
   has_many :tags
+
+  def short_content
+    "#{object.content[0..39]}..."
+  end
 end
--- app/serializers/tag_serializer.rb
+++ app/serializers/tag_serializer.rb
@@ -2,4 +2,5 @@ class TagSerializer < ActiveModel::Serializer
   attributes :name
 
   has_many :posts
+  
 end
